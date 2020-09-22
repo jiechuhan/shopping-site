22a499377e07923f1aa1ef9eca840ebb52e17a85
Chuhan Jie <jiechuhan1996@gmail.com>
2020-09-21 23:08:20 -0700

    json

--- store/src/components/Card/Card.js
+++ store/src/components/Card/Card.js
@@ -6,13 +6,13 @@ import img from '../../assets/img/1.jpg';
 
 import Button from '../Button/Button';
 
-const card = () => {
+const card = (props) => {
     return (
         <div className={`mx-2 my-4 ${classes.card}`}>
             <img src={img} className="card-img-top" alt={`bakery`}></img>
             <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">$50</p>
+                <h5 className="card-title">{props.product.productName}</h5>
+                <p className="card-text">{props.product.productPrice}</p>
             </div>
             <Button btnType="addToCart"><span>Add to cart</span></Button>
         </div>
--- store/src/containers/ProductList/ProductList.js
+++ store/src/containers/ProductList/ProductList.js
@@ -1,41 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react';
+
 import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import classes from './ProductList.module.css';
 
+import data from '../../assets/data/data.js';
 import Navbar from '../../components/Navbar/Navbar';
 import Button from '../../components/Button/Button';
 import Card from '../../components/Card/Card';
 
-const prodcutList = () => {
-    return (
-        <React.Fragment>
-            <div className={classes.sticky}>
-                <Navbar className={classes.nav} />
-            </div>
+class ProductList extends Component {
+    // state = {
+    //     products: [
+    //         {
+    //             productId: "1",
+    //             productName: 'item1',
+    //             productPrice: "$40",
+    //             productImage: null,
+    //             productUrl: null,
+    //             productTitle: "title1",
+    //             productDescription: "description1"
+    //         }
+    //     ]
+    // };
 
-            <div className={classes.container}>
-                <h1 className={classes.order}>Order</h1>
+    state = { products: data};
 
-                <div className="row">
-                    <div className={`col-sm-3 ${classes.dropDownMenu}`}>
-                        <Button btnType="dropdown-item">PICK-UP</Button>
-                        <Button btnType="dropdown-item">DELIVERY</Button>
-                        <Button btnType="dropdown">CURBSIDE PICK-UP</Button>
-                    </div>
+    render() {
 
-                    <div class="col-sm-9 d-flex flex-wrap justify-content-center">
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
-                    
+        let products = this.state.products.map(product => {
+            return (
+                <Card product={product}/>
+            );
+        });
 
-                        
+        return (           
+
+            <div>
+                <div className={classes.sticky}>
+                    <Navbar className={classes.nav} />
+                </div>
+    
+                <div className={classes.container}>
+                    <h1 className={classes.order}>Order</h1>
+    
+                    <div className="row">
+                        <div className={`col-sm-3 ${classes.dropDownMenu}`}>
+                            <Button btnType="dropdown-item">PICK-UP</Button>
+                            <Button btnType="dropdown-item">DELIVERY</Button>
+                            <Button btnType="dropdown">CURBSIDE PICK-UP</Button>
+                        </div>
+    
+                        <div class="col-sm-9 d-flex flex-wrap justify-content-center">  
+                            {products}
+                        </div>
                     </div>
                 </div>
             </div>
-        </React.Fragment>
-    );
+        )
+    };
 };
 
-export default prodcutList;
+export default ProductList;
